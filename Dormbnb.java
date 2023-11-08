ba9a388424d49a9c73d71271e07788b488885e09
Bamo0507 <bryanalbertomo@gmail.com>
2023-11-08 10:26:46 -0600

    Se ha incluido el método para enviar correos, junto con las librerías que se deben importar
    Co-Authored-By: adrianap0607 <132533905+adrianap0607@users.noreply.github.com>

--- Dormbnb.java
+++ Dormbnb.java
@@ -7,7 +7,14 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
+import java.util.Properties;
 import java.util.regex.*;
+
+import javax.mail.MessagingException;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URI;
@@ -17,6 +24,10 @@ import java.util.Date;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class Dormbnb {
     private static boolean valid = true;
@@ -836,4 +847,41 @@ public static void subMenuC(boolean logIn, Comprador comprador, String submenuC,
             }
     }
 
+    public static void enviarCorreo(String destinatario, String mensaje) {
+        // Datos de la cuenta de correo
+        String remitente = "DormAirBnb@gmail.com";
+        String password = "D43A183F9933CDEB57BDFA863B41F50E08DE";
+
+        // Propiedades para la conexión SMTP
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.elasticemail.com");
+        props.put("mail.smtp.port", "2525");
+
+        // Inicio de sesión
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(remitente, password);
+            }
+        });
+
+        try {
+            // Crear un mensaje de correo
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(remitente));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destinatario));
+            message.setSubject("Solicitud de Dorm");
+            message.setText(mensaje);
+
+            // Enviar el mensaje
+            Transport.send(message);
+
+            System.out.println("El correo ha sido enviado exitosamente.");
+
+        } catch (MessagingException e) {
+            System.err.println("Error al enviar el correo: " + e.getMessage());
+        }
+    }
 }
